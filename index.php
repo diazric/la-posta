96fe45682f28e4656954f2a2667ead58a32bad4f
Ricardo Diaz <diazric@gmail.com>
2017-02-23 14:00:50 -0400

    Email
    Set email

---
+++ index.php
@@ -0,0 +1,529 @@
+  <?php 
+     
+     function sendEmail(){
+      if(!empty($_POST)){
+        mail('diazric@gmail.com', $_POST['email'], $_POST['message'] . $_POST['name']);
+      }
+    }
+   ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>La Posta Bar</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="author" content="La Posta Bar">
+    <meta name="description" content="El mejor bar de Republica Dominicana, con una gran variedad de cocteles y un amplio menu."/>
+  </head>
+
+  <body>
+
+    <!-- Nav bar --> 
+
+    <nav class="black" role="navigation">
+      <div class="nav-wrapper container">
+
+
+        <ul class="center hide-on-med-and-down light">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#nosotros">Nosotros</a></li>
+          <li><a href="#menu">Menú</a></li>
+          <li><a href="#galeria">Galeria</a></li>
+          <li><a href="#reservas">Reservas</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+
+
+        <ul id="nav-mobile" class="side-nav light">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#nosotros">Nosotros</a></li>
+          <li><a href="#menu">Menú</a></li>
+          <li><a href="#galeria">Galeria</a></li>
+          <li><a href="#reservas">Reservas</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+    <!-- Nav bar END --> 
+
+    <!-- Container -->
+    <div id="index-banner" class="parallax-container">
+      <div class="parallax-container">
+        <div class="parallax"><img src="header2.jpg" alt="Unsplashed background img 1"></div>
+      </div>
+    </div>
+    <!-- Container End -->
+
+
+    <div class="black" itemscope itemtype ="https://schema.org/LocalBusiness">
+      <div class="container">
+        <div class="section">
+          <div class="row menu">
+            <div class="col s12 m6 l6">
+             <img class="responsive-img left" src="about.jpg" >
+           </div>
+
+           <div id="nosotros" class="col s12 m6 l6">
+            <br>
+            <hr>
+            <p class="white-text light no-pad-bot"><span itemprop="name">LA POSTA BAR</span> EL SITIO PARA VER Y DEJARSE VER EN SANTO DOMINGO DONDE PODRÁS DISFRUTAR DE NUESTROS EMBLEMÁTICOS CÓCTELES Y DE LAS MEJORES BEBIDAS LOCALES E INTERNACIONALES EN UN AMBIENTE MODERNO Y TOTALMENTE ABIERTO CON LA MEJOR MÚSICA DEL MOMENTO.</p>
+            <hr><br>
+            <a class="waves-effect waves-yellow btn-large white black-text" href="#contacto">Contacto</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Act -->
+  <div class="parallax-container valign-wrapper ">
+   
+      <div class="container">
+       <section>
+          <div class="row center">
+          <div class="col s12 m12 l12">
+            <h3 class="black-text amber lighten-5 center z-depth-4" style="padding-top: 11px; padding-bottom: 11px;">M&uacutesica en vivo</h3>
+          </div>
+          
+          <div class="col s12 m12 l6 black-text">
+            <div class="card-panel amber lighten-5 z-depth-4">
+              <div class="row">
+                <div><h3 class="light">DOMINGOS</h3></div>
+                <div><h5>Edward Cruz</h5>
+                  <p>A partir de las 8:30 p.m. (BANDA)</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m12 l6 black-text">
+            <div class="card-panel amber lighten-5 z-depth-4">
+              <div class="row">
+                <div><h3 class="light">LUNES</h3></div>
+                <div><h5>Edward Cruz</h5>
+                  <p>A partir de las 8:30 p.m. (SOLO)</p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+       </section>
+      </div>
+
+    <div class="parallax">
+      <img src="./images/outside.jpg" alt="Unsplashed background img 3">
+    </div>
+
+  </div>
+
+
+  <!-- Act End -->
+
+
+  <!-- MENU -->
+  <div class="black" itemscope itemtype="https://schema.org/Restaurant">
+    <!-- FINGER FOOD -->
+    <div class="z-depth-2 white-text black">
+      <div class="container loco">
+        <div class="row">
+          <div id="menu" class="col s12 m12 l12">
+            <h1 class="light center"><span itemprop="menu">Menú</span></h1>
+            <span><hr></span>
+            <h3 class="light">FINGER FOOD</h3>
+          </div>
+
+          <div class="col sq12 m6 l6 light">
+            <h5>ACEITUNAS RELLENAS</h5>
+            <span>Aceitunas fritas rellenas de queso de cabra y trufas</span>
+          </div>
+          <div class="col s12 m6 l6 light">
+           <h5>EDAMAME & SEA SALT</h5>
+           <span style="visibility: hidden;">2</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>PATO A LA PEKINES</h5>
+           <span>Pato conditado / Crepe / Hoisin / Puerro / Pepino</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>CALAMARES A LA PUTANESCA</h5>
+           <span>Calamares fritos / Tomate / Aceitunas / Ajo / Albahaca / Cebolla / Alcaparra</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>BRUCHETTA DE CIABATTA</h5>
+           <span>Tomates frescos / Taleggio / Pesto genovesse</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>PHILLY STEAK SPRING ROLLS</h5>
+           <span>Tenderloin con cebolla caramelizada / Hongos / Cheddar y Provolone</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>CROQUETAS DE RISOTTO</h5>
+           <span>Rellenas de camarón & Pesto / Crema de parmesano</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>EGG ROLL A LA CUBANA</h5>
+           <span>Pierna de cerdo / Queso suizo / Jamón / Honey mustard</span>
+         </div>
+       </div>
+     </div>
+
+
+     <!-- TO SHARE -->
+
+     <div class="black">
+      <div class="container loco2">
+        <div class="row">
+          <div class="col s12 m12 l12">
+
+            <span><hr></span>
+            <h3 class="light">TO SHARE</h3>
+          </div>
+
+          <div class="col sq12 m6 l6 light">
+            <h5>TABLA DE EMBUTIDOS</h5>
+            <span>Prosciutto / Salami / Speck / Chorizo / Ciabatta / Focaccia</span>
+          </div>
+          <div class="col s12 m6 l6 light">
+           <h5>TABLA DE QUESO</h5>
+           <span>Mozzarella / Grana padano / Taleggio / Gorgonzola / Ciabatta / Focaccia</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>SALCHICHA ITALIANA ASADA</h5>
+           <span>Ensalada de tomate / Perejil / Chimichurri Italiano</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>SALCHICHAS MIXTAS</h5>
+           <span>Italiana / Argentina / Picante / Truffle mayo / Honey Mustard</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>DIM SUM</h5>
+           <span>Dumplings de cerdo al vapor & Sellados con Truffle Ponzu & Sweet chilli</span>
+         </div>
+         <div class="col s12 m6 l6 light">
+           <h5>PINCHO DE CAMARONES</h5>
+           <span>Peperonata asada / Chimichurri italian Picante</span>
+         </div>
+
+       </div>
+     </div>
+
+   </div>
+
+   <!-- TO SHARE END-->
+
+
+   <!-- PIZZA -->
+
+   <div class="black">
+    <div class="container loco3">
+      <div class="row">
+        <div class="col s12 m12 l12">
+
+          <span><hr></span>
+          <h3 class="light">PIZZAS</h3>
+        </div>
+
+        <div class="col sq12 m6 l6 light">
+          <h5>FRESCO</h5>
+          <span>Tomate Fresco / Mozzarella Fresca / Oregano Fresco</span>
+        </div>
+        <div class="col s12 m6 l6 light">
+         <h5>VERDURE</h5>
+         <span>Hongos / Berenjena / Zucchine / Ajíes / Pesto Genovese</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>PARMA</h5>
+         <span>Prosciutto / Rucola / Tomate Seco / Lascas de Parmesano / Vincotto</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>SALSICCIA</h5>
+         <span>Salsiccia Italiana / Espinaca / Cebollino</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>IL FINO</h5>
+         <span>Speck Ahumado / Queso Pecorino / Hongos / Rucola / Trufa</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>BIANCA</h5>
+         <span>Tres Quesos / Cebolla Caramelizadas / Peras al Vino / Pistaccio / Speck</span>
+       </div>
+
+     </div>
+   </div>
+   <br>  
+  </div>
+
+  <!-- PIZZA END-->
+
+  <!-- TEX-MEX -->
+
+  <div class="black">
+    <div class="container loco4">
+      <div class="row">
+        <div class="col s12 m12 l12">
+
+          <span><hr></span>
+          <h3 class="light">TEX-MEX</h3>
+        </div>
+
+        <div class="col sq12 m6 l6 light">
+          <h5>PULLES PORK BBQ TACOS</h5>
+          <span>Salsa BBQ / Guacamole / Ensalada de Cilantro / Chicharrón</span>
+        </div>
+        <div class="col s12 m6 l6 light">
+         <h5>QUESADILLA DE CHURRASCO</h5>
+         <span>Ropa Vieja / Tortilla de Trigo / Guacamole / Fondue de Queso</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>TUTI'S CHICHARRON NACHOS</h5>
+         <span>Carne de Res & Pulled Pork / Guacamole / Queso de Cabra / Pico de Gallo / Crema Agria / Jalapeño / Chicharrón</span>
+       </div>
+
+     </div>
+   </div>
+   <br>  
+  </div>
+
+  <!-- TEX- MEX END-->
+
+  <!-- RAW BAR -->
+
+  <div class="black">
+    <div class="container loco5">
+      <div class="row">
+        <div class="col s12 m12 l12">
+          <span><hr></span>
+          <h3 class="light">RAW BAR</h3>
+        </div>
+
+        <div class="col sq12 m6 l6 light">
+          <h5>TARTAR DE SALMON -ADD TACO</h5>
+          <span>Aceite de Oliva / Ajonjoli / Puerro / Pepino Horseradish / Cheese Toast</span>
+        </div>
+        <div class="col s12 m6 l6 light">
+         <h5>TARTAR DE TUNA -ADD TACO</h5>
+         <span>Salsa Ponzu / Trufa / Aguacate / Lechuga / Casabe</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>RICELESS SALMON ROLL</h5>
+         <span>Aguacate, Trufa & Cream Cheese / Quinoa Tostada / Rábano / Teriyaki</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>NAGASAKI CARPACCIO</h5>
+         <span>Tenderloin / Ponzu / Trufa / Ensalada Thai / Cilantro</span>
+       </div>
+       <div class="col s12 m6 l6 light">
+         <h5>CEVICHE PERUANO CL&AacuteSICO -ADD TACO</h5>
+         <span>Carite / Ají Amarillo Peruano / Batata / Maíz Crocante / Cilantro / Lechuga / Cebolla Roja</span>
+       </div>
+
+     </div>
+   </div>
+   <br>  
+  </div>
+
+  <!-- RAW BAR END-->
+  </div>
+
+  <!-- MENU END -->
+
+  </div>
+
+
+  <!-- GALLERY -->
+
+
+  <div class="black section" id="galeria">
+    <div class="row container loco6 ">
+
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="./images/pizza.jpg">
+            <div class="card-content amber lighten-5">
+              <p>Pizza | IL FINO</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="./images/Sauces.jpg">
+            <div class="card-content amber lighten-5">
+              <p>Salchichas</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image waves-effect ">
+            <img class="responsive-img" src="./images/pizzafresco.jpg">
+            <div class="card-content amber lighten-5">
+              <p>Pizza | Fresco</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="./images/drink1.jpg">
+          <div class="card-content amber lighten-5">
+            <p>Trago 1</p>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4">
+        <div class="card">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="./images/drink2.jpg">
+          <div class="card-content amber lighten-5">
+            <p>Trago 2</p>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4">
+        <div class="card no-padding">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="./images/test.jpg">
+          
+          <div class="card-content amber lighten-5">
+            <p>test</p>
+          </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <!-- GALLERY END -->
+  <!--FORM -->
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <div class="amber lighten-5 section">
+    <div class="container">
+    <h2>Contactos</h2>
+    <div class="col s12 m12 l12">
+      
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate" name="name">
+          <label for="last_name">Nombre y apellido</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="email" type="email" class="validate" name="email">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+      <div class="input-field col s6">
+        <textarea id="textarea1" class="materialize-textarea" name="message"></textarea>
+        <label for="textarea1">Mensaje</label>
+      </div>
+      <div class="input-field col s6">
+        <input class="btn waves-effect waves-light" type="submit" name="action" onclick="<?php sendEmail();?>">
+    </button>
+      </div>
+        
+      </div>
+    </div>
+  </div>
+  </div>
+      </form>
+
+  <!-- FORM End -->
+
+
+  <!-- FOOOTER -->
+
+  <footer id="reservas" class="page-footer black">
+    <div class="container" id="contacto">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text light" itemscope itemtype ="http://schema.org/Organization">CONTACTO</h5>
+          <p class="grey-text text-lighten-4 light" itemprop="address">Gustavo Mej&iacutea Ricart #122 Esq. Manuel de Jes&uacutes Troncoso Ens. Piantini. <br>C. <a href="mailto:reservas@lapostabar.com?Subject=Reservas%20La%20Posta" itemprop="email"  target="_top">Contacto</a><br> <span itemprop="telephone">T. 809 475 1007</span></p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text light">REDES SOCIALES</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3 light" href="https://www.facebook.com/La-Posta-Bar-483396758417640/" target="_blank">Facebook</a></li>
+            <li><a class="grey-text text-lighten-3 light" href="https://www.instagram.com/lapostabar/?hl=en" target="_blank">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright black">
+      <div class="container center light">
+        <span style="font-size: .8em;">© 2017 THE FOODHALL - LA POSTA BAR | Design by <a href="https://www.ricardodiazm.net">Ricardo Diaz</a></span>
+      </div>
+    </div>
+  </footer>
+
+  <!--footer end -->
+
+
+  <!--Import jQuery before materialize.js-->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <script type="text/javascript">
+    var options = [
+    {selector: '.menu', offset: 200, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '.loco', 
+    offset: 200, 
+    callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '.loco2', offset: 200, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '.loco3', offset: 200, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '.loco4', offset: 200, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },{selector: '.loco5', offset: 200, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } },
+    {selector: '.loco6', offset: 100, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } }
+
+    ]; Materialize.scrollFire(options);
+  </script>
+
+  <script type="text/javascript">
+
+    $( document ).ready(function(){
+      $('.parallax').parallax();
+      $('.carousel.carousel-slider').carousel({full_width: true});
+      $(".button-collapse").sideNav({closeOnClick: true});
+      $('.scrollspy').scrollSpy();
+
+
+    });
+  </script>
+  </body>
+  </html>
